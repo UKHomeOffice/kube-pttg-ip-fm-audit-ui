e7530bc74e45e9615774a59c3fa6adb77cb3b50f
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-07-19 14:31:35 +0100

    Migrate app to ACP

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,48 @@
-#!/usr/bin/env bash
-export WHITELIST=${WHITELIST:-0.0.0.0/0}
+#!/bin/bash
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
-    export DNS_PREFIX=
-else
-    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
 fi
 
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    export KUBE_TOKEN=${PTTG_IP_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_IP_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_IP_DEV}
+    fi
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+export WHITELIST=${WHITELIST:-0.0.0.0/0}
+
+if [ "${ENVIRONMENT}" == "pr" ] ; then
+    export DNS_PREFIX=
+    export KC_REALM=pttg-production
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+    export KC_REALM=pttg-qa
+fi
+
+export DOMAIN_NAME=ip.${DNS_PREFIX}pttg.homeoffice.gov.uk
+
+echo "DOMAIN_NAME is $DOMAIN_NAME"
+
 cd kd
-kd --insecure-skip-tls-verify --timeout 5m0s \
-   --file ingress.yaml \
-   --file service.yaml \
-   --file deployment.yaml
+
+kd --insecure-skip-tls-verify \
+    -f networkPolicy.yaml \
+    -f ingress.yaml \
+    -f deployment.yaml \
+    -f service.yaml
